31038a8cd1840c55f8979feac1da5216b13fbfd9
onesmas <onesmas78@gmail.com>
2023-07-30 23:54:56 +0300

    Updated gradle dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,12 +11,13 @@ repositories {
 
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation ("com.sparkjava:spark-core:2.9.3")
     implementation ("com.sparkjava:spark-template-handlebars:2.7.1")
     implementation ("com.github.jknack:handlebars:4.3.0")
     implementation ("org.slf4j:slf4j-simple:1.7.36")
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
 }
 
 tasks.test {
