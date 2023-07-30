36259fe921cc98c5404e1e9feaeb53d74edc43b4
onesmas <onesmas78@gmail.com>
2023-07-30 23:40:40 +0300

    Squad testing package

---
+++ src/test/java/squadtest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@SuppressWarnings("ALL")
+public class squadtest {
+
+    @Test
+    private hero setupNewHero() {
+        return new hero("Superman", 35, "Superstrength", "Kryptonite");
+    }
+
+    @Test
+    private squad setupNewSquad(hero hero) {
+        return new squad("Justice League", "Save Humanity", hero);
+    }
+
+    @Test
+    public void squadInstanciatedCorrectly() {
+        squad squad = setupNewSquad(setupNewHero());
+        assertTrue(squad instanceof squad);
+    }
+
+    @Test
+    public void getSquadName() {
+        squad squad = setupNewSquad(setupNewHero());
+        assertTrue(squad.getName() instanceof String);
+    }
+
+    @Test
+    public void getSquadCause() {
+        squad squad = setupNewSquad(setupNewHero());
+        assertTrue(squad.getCause() instanceof String);
+    }
+}
